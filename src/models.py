13a677b334b56ffc31d4efcedcab7abf09c3dfd9
marcndo <ndowahmarcel@gmail.com>
2026-08-11 22:31:47 +0100

    feat(ebook): add Ebook subclass of AbstractBook to demonstrate polymorphism

--- src/models.py
+++ src/models.py
@@ -1,4 +1,19 @@
-class Book:
+from abc import ABC, abstractmethod
+
+
+
+class AbstractBook(ABC):
+
+    @abstractmethod
+    def borrow(self):
+        pass
+
+    @abstractmethod
+    def return_book(self):
+        pass
+
+
+class Book(AbstractBook):
     def __init__(self, title, author, isbn):
         self._title = title
         self.author = author
@@ -69,6 +84,56 @@ class Library:
             raise ValueError("Book not available")
         book.borrow()
         member.borrowed_books.append(book)
+
+class Ebook(AbstractBook):
+    def __init__(self, title, author, isbn, file_size_mb):
+        self._title = title
+        self.author = author
+        self._isbn = isbn
+        self.file_size_mb = file_size_mb
+        self._is_borrowed = False
+
+    def borrow(self):
+        if self.is_borrowed:
+            raise ValueError(f"{self.title} already borrowed")
+        self._is_borrowed = True
+
+
+    def return_book(self):
+        self._is_borrowed = False
+    
+    @property
+    def isbn(self):
+        return self._isbn
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, new_title):
+        if not isinstance(new_title, str):
+            raise ValueError("Check to ensure you enter the correct title")
+        new_title = new_title.strip()
+        if not new_title:
+            raise ValueError("Check to ensure you enter the correct title")
+        self._title = new_title
+
+    @property
+    def is_borrowed(self):
+        return self._is_borrowed
+
+    def __repr__(self):
+        return f"Ebook('{self.title}', '{self.author}', {self.file_size_mb}MB)"
+    
+def show_status(book: AbstractBook):
+    print(f"{book.title} - borrowed: {book.is_borrowed}")
+
+
+    
+    
+
+
 
 
 
