36e459d110abf083d4a13d0a19ccf5e8e903a29d
marcndo <ndowahmarcel@gmail.com>
2026-09-02 13:53:39 +0100

    feat(logging): add file handler alongside console output

--- src/logger.py
+++ src/logger.py
@@ -10,4 +10,8 @@ def get_logger(name: str) -> logging.Logger:
         )
         handler.setFormatter(formatter)
         logger.addHandler(handler)
+        file_handler = logging.FileHandler("app.log")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
     return logger
