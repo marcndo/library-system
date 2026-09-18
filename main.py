0ddfeff73d8758fcd0717b7e22fae4af5e777e90
marcndo <ndowahmarcel@gmail.com>
2026-09-18 14:30:36 +0100

    feat(fastapi): redirect the root directory to the home page

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 from src.book import Book
 from src.user import User
 from fastapi import Request
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, redirectResponse
 from sqlalchemy.exc import IntegrityError
 from datetime import datetime, timedelta, timezone
 from fastapi.security import OAuth2PasswordRequestForm,  OAuth2PasswordBearer
@@ -76,7 +76,7 @@ class UserCreateRequest(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"message": "Library System API"}
+    return redirectResponse(url="/static/index.html")
 
 @app.get("/books")
 def get_books():
